17c0925fe90a9126d44d464daf5ef04c34bd6c3a
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-07-28 21:43:03 +0530

    like ui changes

--- client/src/components/HomeCard.jsx
+++ client/src/components/HomeCard.jsx
@@ -149,7 +149,7 @@ const HomeCard = (props) => {
           <div className='flex mt-4 items-center gap-5 p-2'>
             <div className="flex items-center text-muted-foreground">
               <Button variant="ghost" onClick={handleLike} className='flex items-center gap-1'>
-                <HeartIcon className={isLiked ? "h-5 w-5 fill-current text-blue-500" : "h-5 w-5"} />
+                <HeartIcon className={isLiked ? "h-5 w-5 fill-current text-[#e12f6d]" : "h-5 w-5"} />
                 <span>{numberOfLikes}</span>
               </Button>
              
